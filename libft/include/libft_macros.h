b5f7e9b549352824f4cdfcc13919c479e1194eeb
Victor Wade <viwade@student.42.us.org>
2020-01-20 12:27:38 -0800

    the woes of deleted text
    the criminality of a crawling vim

--- libft/include/libft_macros.h
+++ libft/include/libft_macros.h
@@ -6,15 +6,25 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/03 00:37:49 by viwade            #+#    #+#             */
-/*   Updated: 2020/01/19 11:07:20 by viwade           ###   ########.fr       */
+/*   Updated: 2020/01/20 12:10:53 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LIBFT_MACROS_H
 # define LIBFT_MACROS_H
 # include <stdlib.h>
+# include <stdint.h>
 # include <sys/types.h>
 # include "libft.h"
+
+typedef void *	t_vptr;
+typedef char *	t_cptr;
+typedef int *	t_iptr;
+typedef double	t_db;
+typedef long long	t_ll;
+typedef long double	t_ld;
+typedef unsigned long long	t_ull;
+typedef unsigned long	t_ul;
 
 /*
 **	--	DATA TYPE DEFINITIONS	--
@@ -27,18 +37,17 @@
 # define FT_NBR		t_num
 # define FT_STR		caddr_t
 # define FT_WSTR	caddr_t
-# define FT_LSTR	int *
+# define FT_LSTR	intptr_t
 # define FT_TSTR	t_str
 # define FT_INT		int
-# define FT_UINT	UINT
-# define FT_LIST	t_list *
+# define FT_UINT	uint
+# define FT_LIST	t_list
 # define FT_VOID	void
-# define FT_VOIDP	void *
-# define FT_D		double
-# define FT_LD		long double
+# define FT_D		double_t
+# define FT_LD		double_t
 # define FT_FLOAT	float
-# define FT_LL		LONG
-# define FT_ULL		unsigned long long
+# define LL_T		int64_t
+# define ULL_T		u_int64_t
 
 /*
 **	--	DATA TYPE SHORTHAND	--
@@ -59,21 +68,36 @@
 
 /*
 **	TYPE DEFINITION
+**	define _TD_LD	typedef long double ld_t
+**	define _TD_D	typedef double	db_t
+**	define _TD_LL	typedef long long	ll_t
+**	define _TD_ULL	typedef unsigned long long	ull_t
+**	define _TD_BSTR	typedef struct s_byte_string bstr_t
 */
 
-# define _TD		typedef
-# define _TD_LD		long double
-# define _TD_D		typedef double	db_t
-# define _TD_LL		typedef long long	ll_t
-# define _TD_ULL	typedef unsigned long long	ull_t
-# define _TD_BSTR	typedef struct s_byte_string bstr_t
+# ifdef _TD_LD
 
-// _TD int	i32_t;
-_TD;
 _TD_LD;
+# endif
+
+# ifdef _TD_D
+
 _TD_D;
+# endif
+
+# ifdef _TD_LL
+
 _TD_LL;
+# endif
+
+# ifdef _TD_ULL
+
 _TD_ULL;
+# endif
+
+# ifdef _TD_BSTR
+
 _TD_BSTR;
+# endif
 
 #endif
