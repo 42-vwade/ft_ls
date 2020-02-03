638d60103ee69a713e2601de76e1da287c48f695
Victor Wade <viwade@student.42.us.org>
2020-02-02 18:04:58 -0800

    reverse sort added for multiple input

--- source/mode/ls_collect_jobs.c
+++ source/mode/ls_collect_jobs.c
@@ -6,7 +6,7 @@
 /*   By: viwade <viwade@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/15 19:57:12 by viwade            #+#    #+#             */
-/*   Updated: 2020/01/30 21:23:24 by viwade           ###   ########.fr       */
+/*   Updated: 2020/02/02 18:03:22 by viwade           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,17 @@ int
 	return (ft_strcmp(left, right) > 0);
 }
 
+int
+	ls_rnamesort(t_node *a, t_node *b)
+{
+	char	*left;
+	char	*right;
+
+	left = a->content;
+	right = b->content;
+	return (ft_strcmp(left, right) < 0);
+}
+
 t_node
 	*ls_collect_paths(t_param *p)
 {
@@ -34,6 +45,8 @@ t_node
 	paths = 0;
 	while (*v || ((node = paths) && 0))
 		ls_node_append(&paths, ls_node_new(ls_strtrim(*v++)));
-	paths = ls_merge_sort(paths, ls_namesort);
+	paths && ((p->f.r &&
+	((paths = ls_merge_sort(paths, ls_rnamesort)) || 1)) ||
+	((paths = ls_merge_sort(paths, ls_namesort))));
 	return (paths);
 }
